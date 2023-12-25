3c995d7ef5c4225a2a1e3154f6fc3c94427e26a3
biswasprasana001 <biswasprasana004@gmail.com>
2023-12-25 13:41:54 +0530

    Setup Express Server, Connected to MongoDB

---
+++ server/server.js
@@ -0,0 +1,24 @@
+// server.js
+
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+const MONGO_URI = process.env.MONGO_URI;
+
+// Connect to MongoDB
+mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Successfully connected to MongoDB'))
+  .catch(err => console.error('Connection error', err));
+
+// Basic route for the home page
+app.get('/', (req, res) => {
+  res.send('Hello World! This is the home page.');
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
