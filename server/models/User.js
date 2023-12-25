f14b998d5a27ca9bb84130386c2342e3220b6564
biswasprasana001 <biswasprasana004@gmail.com>
2023-12-25 22:08:43 +0530

    Created React App, Implemented User Modal

---
+++ server/models/User.js
@@ -0,0 +1,45 @@
+// models/User.js
+
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+// User Schema
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, 'Username is required'],
+        trim: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required'],
+        trim: true,
+        unique: true,
+        lowercase: true,
+        match: [/\S+@\S+\.\S+/, 'Please use a valid email address'],
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+        minlength: [8, 'Password must be at least 8 characters long'],
+    }
+    // Add additional fields as needed
+});
+
+// Pre-save hook to hash password before saving to database
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        this.password = await bcrypt.hash(this.password, salt);
+        next();
+    } catch (error) {
+        next(error);
+    }
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
